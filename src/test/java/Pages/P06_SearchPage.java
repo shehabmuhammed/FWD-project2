3d9fbc061f81555a2fd3ad837e8ca124860d1054
shehabmuhammed <106969736+shehabmuhammed@users.noreply.github.com>
2022-07-25 20:50:09 +0200

    Add files via upload

---
+++ src/test/java/Pages/P06_SearchPage.java
@@ -0,0 +1,27 @@
+package Pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.PageFactory;
+
+public class P06_SearchPage {
+
+        WebDriver driver;
+
+        public P06_SearchPage(WebDriver driver) {
+            this.driver = driver;
+            PageFactory.initElements(driver,this);
+        }
+
+        public int productsList(){
+            int size = driver.findElements(By.cssSelector("h2[class=\"product-title\"]")).size();
+            return size;
+        }
+
+        public WebElement noProducts(){
+            return driver.findElement(By.cssSelector("div[class=\"no-result\"]"));
+        }
+    }
+
+
